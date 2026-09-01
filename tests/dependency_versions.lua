61d9d411bfdd7e6d1545fa5dd57aebc7445394cd
Michał 'Griwes' Dominiak <griwes@griwes.info>
2026-09-01 15:30:31 -0700

    Harden reproducible CI coverage.

---
+++ tests/dependency_versions.lua
@@ -0,0 +1,10 @@
+return {
+    lazy = '306a05526ada86a7b30af95c5cc81ffba93fef97',
+    plenary = 'b9fd5226c2f76c951fc8ed5923d85e4de065e509',
+    coverage = 'a939e425e363319d952a6c35fb3f38b34041ded2',
+    conform = '016802de402556da54c36bd7359b441266b01cdd',
+    mason = '2a6940af80375532e5e9e7c1f2fc6319a1b7a69d',
+    dap = '9e848e09a697ee95302a3ef2dd43fd6eb709e570',
+    lint = '3d55c8f67c6ae5c15e1042571e107c7a3d5c5f4e',
+    overseer = 'a93d9f6d6defdac4bcd6d2c8ba988650e42e0a0e',
+}
--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -4,9 +4,18 @@ vim.g.maplocalleader = ' '
 vim.opt.runtimepath:prepend(vim.fn.getcwd())
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local dependency_versions = dofile('tests/dependency_versions.lua')
+
+local function run_git(args)
+    local output = vim.fn.system(args)
+    if vim.v.shell_error ~= 0 then
+        error(table.concat(args, ' ') .. '\n' .. output)
+    end
+    return output
+end
 
 if vim.fn.isdirectory(lazypath) == 0 then
-    vim.fn.system({
+    run_git({
         'git',
         'clone',
         '--filter=blob:none',
@@ -16,17 +25,22 @@ if vim.fn.isdirectory(lazypath) == 0 then
     })
 end
 
+local lazy_head = vim.trim(run_git({ 'git', '-C', lazypath, 'rev-parse', 'HEAD' }))
+if lazy_head ~= dependency_versions.lazy then
+    run_git({ 'git', '-C', lazypath, 'checkout', '--force', '--detach', dependency_versions.lazy })
+end
+
 vim.opt.runtimepath:prepend(lazypath)
 
 require('lazy').setup({
     { dir = vim.fn.getcwd(), lazy = false },
-    { 'nvim-lua/plenary.nvim', lazy = false },
-    { 'andythigpen/nvim-coverage', lazy = false },
-    { 'stevearc/conform.nvim', lazy = false },
-    { 'williamboman/mason.nvim', lazy = false },
-    { 'mfussenegger/nvim-dap', lazy = false },
-    { 'mfussenegger/nvim-lint', lazy = false },
-    { 'stevearc/overseer.nvim', lazy = false },
+    { 'nvim-lua/plenary.nvim', commit = dependency_versions.plenary, lazy = false },
+    { 'andythigpen/nvim-coverage', commit = dependency_versions.coverage, lazy = false },
+    { 'stevearc/conform.nvim', commit = dependency_versions.conform, lazy = false },
+    { 'williamboman/mason.nvim', commit = dependency_versions.mason, lazy = false },
+    { 'mfussenegger/nvim-dap', commit = dependency_versions.dap, lazy = false },
+    { 'mfussenegger/nvim-lint', commit = dependency_versions.lint, lazy = false },
+    { 'stevearc/overseer.nvim', commit = dependency_versions.overseer, lazy = false },
 }, {
     root = vim.fn.stdpath('data') .. '/lazy',
     lockfile = vim.fn.stdpath('state') .. '/lazy-lock.json',
